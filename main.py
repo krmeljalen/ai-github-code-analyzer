9e8eeea4073e00ccfb44848916001366e21b26f1
Alen Krmelj <krmelj.alen@gmail.com>
2024-08-19 21:39:22 +0200

    Fixed main

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from utils.session_state import SessionState
 
 
 def chat_loop(session_state):
+    if not session_state.query_engine:
+        logs.log.error(f"Query engine not set.")
+        sys.exit(1)
+
     while True:
         user_input = input("Question: ")
         if user_input.strip().upper() == "EOF":
@@ -14,7 +18,7 @@ def chat_loop(session_state):
             break
 
         ollama.context_chat(prompt=user_input, query_engine=session_state.query_engine)
-
+        print()
 
 def run(repo="krmeljalen/kdeploy"):
     session_state = SessionState()
